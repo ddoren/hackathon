7d1ecee8032902c398db9b29db30dbb8fad7ad1a
dim <dimitris.giokas1@gmail.com>
2021-11-06 19:22:14 +0100

    initial commit

---
+++ scraper.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+database_url = 'https://www.epddanmark.dk/epd-databasen/'
+
+
+base_url = 'https://www.epddanmark.dk'
+
+
+url_list = []
+
+
+page = requests.get(database_url)
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.findAll('div', {'class': 'small-12 medium-12 large-12 columns'})
+for r in results:
+    a_list = r.findAll('a')
+    if len(a_list) > 0:
+        href = a_list[1]['href']
+        if not href.startswith('/media'):
+            continue
+        url_list.append(base_url + href)
+
